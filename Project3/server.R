4cf5799f8d6438e198989b5d1c914c674444737c
jzhang96 <jzhang96@ncsu.edu>
2022-07-29 15:00:32 -0400

    update for data page

--- Project3/server.R
+++ Project3/server.R
@@ -1,26 +1,71 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(DT)
 library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+library(dplyr)
+library(ggplot2)
+shinyServer(function(input, output, session) {
+  bike = read.csv("C:/Users/LAILA/Desktop/project3/SeoulBikeData.csv")
+  colnames(bike) <- c("Date","Rented Bike count", "Hour", "Temperature", "Humidity",
+                      "Windspeed", "Visibility", "Dew point",
+                      "Solar radiation", "Rainfall", "Snowfall", "Seasons", "Holiday",
+                      "Functional Day")
+  bike$Seasons <- as.factor(bike$Seasons)
+  var <-reactive({
+    value <- input$var
+  })
+  numdata <- reactive({
+    bike[, c("Date","Seasons", "Holiday",
+             "Functional Day", var()),
+         drop = FALSE]
+  })
+  output$table <- renderDataTable({
+    value <- numdata()
+    tab <- aggregate(value[[var()]] ~ Date,
+                     data = value, FUN = summary)
+    tab <- data.frame(location=tab[[1]], tab[[2]])
+    tab[,2:7] <- round(tab[,3], digits = input$rounding)
+    names(tab)[1] <- "Date"
+    names(tab)[2] <- paste("Summary", var())
+    names(tab)[3] <- paste("First Quantile", var())
+    names(tab)[4] <- paste("Median", var())
+    names(tab)[5] <- paste("Average", var())
+    names(tab)[6] <- paste("Third quantile", var())
+    names(tab)[7] <- paste("Maximum", var())
+    print(tab)
+  })
+  output$distPlot <- renderPlot({
+    value <- numdata()
+    if (input$plot == 'Seasons'){
+      ggplot(value, aes(x = Seasons)) +
+        geom_bar()
+    } 
+    else if (input$plot == 'Holiday'){
+      ggplot(value, aes(x = Holiday)) +
+        geom_bar()
+    }
+    else{
+      ggplot(value, aes(x = `Functional Day`)) +
+        geom_bar() 
+    }
+  })
+  
+  output$dt <- 
+    DT::renderDataTable(
+      datatable(bike,
+                filter = "top"),
+      server = FALSE
+    )
+  
+  output$filtered_row <- 
+    renderPrint({
+      input[["dt_rows_all"]]
     })
-
+  
+  output$download_filtered <- 
+    downloadHandler(
+      filename = "Filtered Data.csv",
+      content = function(file){
+        write.csv(bike[input[["dt_rows_all"]], ],
+                  file)
+      }
+    )
 })
--- Project3/ui.R
+++ Project3/ui.R
@@ -1,17 +1,23 @@
+library(DT)
 library(shiny)
+library(shinydashboard)
 ui <- fluidPage(
   titlePanel("Seoul Bike Sharing Demand"),
   
   navlistPanel(
     tabPanel(
       h4("About"),
-      h5("This app is for you to explore the", a(href = "https://archive.ics.uci.edu/ml/datasets/Seoul+Bike+Sharing+Demand", target = "_blank", "Seoul Bike Sharing Demand dataset"), "- originally from the UCI machine learninng repository"),
-      h5("The dataset contains count of public bikes rented at each hour in Seoul Bike haring System with the corresponding Weather data and Holidays information"),
-      h5("There are another three pages."), 
-      h5("In the", strong("Data"), "Exploration page, you can do some numerical and graphical summaries about the dataset."),
-      h5("In the", strong("Modeling"), "page, you can fit three model for the dataset and you will get detailed information about modeling in the Modeling page."),
-      h5("In the", strong("Data"), "page, you can check, subset and download the data.")),
-    
+      mainPanel(
+        h5("This app is for you to explore the", a(href = "https://archive.ics.uci.edu/ml/datasets/Seoul+Bike+Sharing+Demand", target = "_blank", "Seoul Bike Sharing Demand dataset"), "- originally from the UCI machine learninng repository"),
+        h5("The dataset contains count of public bikes rented at each hour in Seoul Bike haring System with the corresponding Weather data and Holidays information"),
+        h5("There are another three pages."), 
+        h5("In the", strong("Data Exploration"), "page, you can do some numerical and graphical summaries about the dataset."),
+        h5("In the", strong("Modeling info"), "page, you can find the detailed information for three models."),
+        h5("In the", strong("Modeling Fitting"), "page, you can fit these three models"),
+        h5("In the", strong("Prediction"), "page, you can do the prediction base on one of the three models"),
+        h5("In the", strong("Data"), "page, you can check, subset and download the data."),
+        tags$img(src='bike.png', width='300px',height='200px')
+      )),
     tabPanel(
       h4("Data Exploration"),
       h4("You can create a few bar plots using radio buttons below."),
@@ -41,6 +47,76 @@ ui <- fluidPage(
         plotOutput("distPlot"),
         dataTableOutput("table")
       )),
-    tabPanel("Modeling "),
-    tabPanel("Data")
-  ))
+    tabPanel(
+      h4("Modeling Info"),
+      strong(h4("Multiple Linear Regression Model")),
+      h5("Multiple linear regression is a regression model that estimates the relationship between a quantitative dependent variable and two or more independent variables using a straight line."),
+      withMathJax(),
+      helpText('The example equation for the Multiple Linear Regression model is
+               $$Y_i = \\beta_0\\ + \\beta_1X_{1i}\\ + \\beta_2X_{2i}\\ + \\beta_3X_{1i}^2\\ + \\beta_4X_{2i}^2\\ + \\beta_5X_{1i}X_{2i}\\ + E_i$$'),
+      helpText('Compare with the simple linear Regression, Multiplt liner regression can have more than one independent variables. And despite the 
+               quadratic term \\(\\beta_3X_{1i}^2\\), we can also include the interaction term \\(\\beta_5X_{1i}X_{2i}\\) in the model.'),
+      strong("Pros"),
+      h5("Multiple linear regression allows the investigator to account for all of these potentially important factors in one model. The advantages of this approach are that this may lead to a more accurate and precise understanding of the association of each individual factor with the outcome."),
+      strong("Cons"),
+      h5("The assumption of linearity between the dependent variable and the independent variables is the main disadvantage since we may falsely concluding that a correlation is a causation"),
+      br(),
+      strong(h4("Classification Trees Model")),
+      h5("A classification tree is a structural mapping of binary decisions that lead to a decision about the class (interpretation) of an object "),
+      h5("The method of this model is Split up predictor space into regions, different predictions for each regioni. For a given region, usually use most prevalent class as prediction"),
+      strong("Pros"),
+      h5("This model is Easy to read and interpret and less data cleaning required."),
+      strong("Cons"),
+      h5("This model has a unstable nature compare to other decision predictors and less effective in predicting the outcome of continuous variable"),
+      br(),
+      strong(h4("Random Forest Model")),
+      h5("Random forest is a type of supervised learning algorithm that uses ensemble methods (bagging) to solve both regression and classification problems. The algorithm operates by constructing a multitude of decision trees at training time and outputting the mean/mode of prediction of the individual trees."),
+      h5("By randomly selecting a subset of predictors, a good predictor or two won't
+dominate the tree fits"),
+      helpText("Usually use \\(m = \\sqrt{p}\\) or \\(m = \\frac{p}{3}\\) randomly selected
+predictors"),
+      strong("Pros"),
+      h5("It can robust to outliers, works well with non-linear data, lower risk of overfitting,
+      runs efficiently on a large dataset and better accuracy than other classification algorithms."),
+      strong("Cons"),
+      h5("It can be quite large, thus making pruning necessary, can't guarantee optimal trees,
+      it gives low prediction accuracy for a dataset as compared to other machine learning algorithms,
+      and calculations can become complex when there are many class variables.")
+    ),
+    tabPanel(
+      h4("Model Fitting"),
+      mainPanel(numericInput(inputId = "split",
+                             label = "Proportion for Training",
+                             value = 0.7,
+                             min = 0.1,
+                             max = 1,
+                             step = 0.1),
+                checkboxGroupInput(inputId = "predictor", 
+                                   label = "Predicotrs used to fit model",
+                                   choices = list("Hour", "Temperature", "Humidity",
+                                                  "Windspeed", "Visibility", "Dew point",
+                                                  "Solar radiation", "Rainfall", "Snowfall", "Seasons", "Holiday",
+                                                  "Functional Day")),
+                strong("Use 5-fold Cross Validation?"),
+                checkboxInput(inputId = "crossv",
+                              label = "Yes"
+                ),
+                actionButton(inputId = "act",
+                             label = "Fit Model"))
+    ),
+    tabPanel(
+      h4("Prediction")),
+    tabPanel(
+      h4("Data"),
+      DT::dataTableOutput("dt"),
+      
+      p("Notice that the 'rows_all' attribute grabs the row indices of the data."),
+      verbatimTextOutput("filtered_row"),
+      
+      
+      downloadButton(outputId = "download_filtered",
+                     label = "Download Filtered Data")
+    )
+  )
+)
+
